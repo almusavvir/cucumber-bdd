9f18b908dd8fd96f00fd7a7c4090108c08f253f1
Al Musavvir <almusavvir@outlook.com>
2024-11-25 00:01:12 +0530

    add loginpage and stepdef

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -1,6 +1,6 @@
 package pageObjects;
 
-import net.bytebuddy.dynamic.scaffold.TypeWriter;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
@@ -8,13 +8,23 @@ public class LoginPage {
     WebDriver driver;
 
     // constructor
-    LoginPage(WebDriver driver) {
+    public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
     // locators
-
+    By usernameLoc = By.xpath("//input[@placeholder='Username']");
+    By passwordLoc = By.xpath("//input[@placeholder='Password']");
+    By loginButtonLoc = By.xpath("//button[normalize-space()='Login']");
 
     // action methods
-
+    public void setUserName(String user) {
+        driver.findElement(usernameLoc).sendKeys(user);
+    }
+    public void setPassword(String password) {
+        driver.findElement(passwordLoc).sendKeys(password);
+    }
+    public void clickLoginButton() {
+        driver.findElement(loginButtonLoc).click();
+    }
 }
--- src/test/java/stepDefinitions/LoginStepDef.java
+++ src/test/java/stepDefinitions/LoginStepDef.java
@@ -1,4 +1,39 @@
 package stepDefinitions;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pageObjects.LoginPage;
+
+import java.time.Duration;
+
 public class LoginStepDef {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void Setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @Test
+    public void LoginTest() {
+        LoginPage lp = new LoginPage(driver);
+        lp.setUserName("Admin");
+        lp.setPassword("admin123");
+        lp.clickLoginButton();
+        Assert.assertEquals(driver.getTitle(), "OrangeHRM");
+    }
+
+    @AfterClass
+    public static void teardown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
 }
