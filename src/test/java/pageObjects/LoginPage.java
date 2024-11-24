b279b21dfe1ead23b7a3d2e7abd0596b5ef134b2
Al Musavvir <almusavvir@outlook.com>
2024-11-24 23:08:30 +0530

    cleaned

---
+++ src/test/java/pageObjects/LoginPage.java
@@ -0,0 +1,20 @@
+package pageObjects;
+
+import net.bytebuddy.dynamic.scaffold.TypeWriter;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    // constructor
+    LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // locators
+
+
+    // action methods
+
+}
